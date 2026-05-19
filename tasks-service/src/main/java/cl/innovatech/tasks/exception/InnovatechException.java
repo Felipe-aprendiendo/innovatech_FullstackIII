14beebb6a4d791349e5bac278f4c604e4779d01e
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2026-05-19 13:02:45 -0400

    Feat: Agrera clases mapper, excepciones y majeno global de ecxepciones

---
+++ tasks-service/src/main/java/cl/innovatech/tasks/exception/InnovatechException.java
@@ -0,0 +1,8 @@
+package cl.innovatech.tasks.exception;
+
+public class InnovatechException extends RuntimeException {
+
+    public InnovatechException(String message) {
+        super(message);
+    }
+}
