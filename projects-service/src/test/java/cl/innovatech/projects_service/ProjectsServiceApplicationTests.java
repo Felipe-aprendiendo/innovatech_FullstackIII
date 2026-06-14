cd252102f4824b24d1caea73377b676c733f70f8
Omar F.M. <om.felipe@duocuc.cl>
2026-06-14 17:57:24 -0400

    step 1: align projects service technical baseline

--- projects-service/src/test/java/cl/innovatech/projects_service/ProjectsServiceApplicationTests.java
+++ projects-service/src/test/java/cl/innovatech/projects_service/ProjectsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package cl.innovatech.projects_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectsServiceApplicationTests {
 
 	@Test
