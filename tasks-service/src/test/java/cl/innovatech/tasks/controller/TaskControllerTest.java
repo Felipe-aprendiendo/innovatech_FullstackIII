64ea9dd45eac107dbe2d0cec5a941e5b7a46dffc
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2026-06-14 08:55:56 -0400

    feat/task-service: Modificaciones para funcionamiento de testcontroller

--- tasks-service/src/test/java/cl/innovatech/tasks/controller/TaskControllerTest.java
+++ tasks-service/src/test/java/cl/innovatech/tasks/controller/TaskControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(controllers = {TaskController.class, CommentController.class})
 class TaskControllerTest {
 
